8af2cc4bc9af5f4b68cd1522354127b54edf23d4
yhc <648697377@qq.com>
2021-04-14 18:45:31 +0800

    add

--- src/pages/table.js
+++ src/pages/table.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import style from "./table.less";
 import classnames from "classnames";
 import { useMount } from "react-use";
@@ -10,26 +10,37 @@ const CustomTable = (props) => {
   const { height = 500, rowHeight = 30, dataSource = data.salaryList } = props;
 
   const [totalH, setTotalH] = React.useState(0);
-  const [transform, setTransform] = React.useState("");
 
   const [dataList, setDataList] = useState([]);
   const [columns, setColumns] = useState([]);
   const [leftColumns, setLeftColumns] = useState([]);
 
+  // 设置滚动区域高度
   useEffect(() => {
     setTotalH(rowHeight * dataSource.length);
   }, [rowHeight, dataSource]);
 
+  //数据初始化
+  const newDataSource = useMemo(function () {
+    return dataSource.map((item, index) => {
+      return {
+        ...item,
+        transform: index * rowHeight
+      }
+    })
+  }, [dataSource])
+
   const virtualList = React.createRef();
   const virtualHeader = React.createRef();
-  const virtualFooter = React.createRef();
   const leftVirtualList = React.createRef();
 
+  // 头部columns
   useEffect(() => {
     setColumns(data.headList);
     setLeftColumns(data.headList.filter((item) => item.fixed));
   }, []);
 
+  // 表单行高样式
   const itemStyle = {
     height: rowHeight,
     lineHeight: `${rowHeight}px`,
@@ -49,19 +60,21 @@ const CustomTable = (props) => {
 
     const updateViewContentFn = () => {
       const clientHeight = mainTableBody.clientHeight;
+      // 计算可视区域里能放几个元素
+      const viewCount = Math.ceil(clientHeight / rowHeight);
+
+      const halfViewCount = Math.ceil(viewCount / 2);
+
       return function (scrollTop = 0) {
-        // 计算可视区域里能放几个元素
-        const viewCount = Math.ceil(clientHeight / rowHeight);
         // 计算可视区域开始的索引
         const initStart = Math.floor(scrollTop / rowHeight);
-        const start = initStart > viewCount ? initStart - viewCount : 0;
+        const start = initStart > viewCount ? initStart - halfViewCount : 0;
         // 计算可视区域结束索引
-        const end = start + viewCount * 3;
+        const end = start + viewCount * 2;
         // 截取可视区域数据
-        const viewData = dataSource.slice(start, end);
+        const viewData = newDataSource.slice(start, end);
 
         setDataList(viewData);
-        setTransform(start * rowHeight);
       };
     };
 
@@ -109,8 +122,9 @@ const CustomTable = (props) => {
     headerTableBody.addEventListener("mouseover", headerMouseFn);
     mainTableBody.addEventListener("mouseover", mainMouseFn);
     leftTableBody.addEventListener("mouseover", leftMouseFn);
-
-    updateViewContent(0);
+    mainTableBody.scrollTop = 0
+    leftTableBody.scrollTop = 0
+    updateViewContent(mainTableBody.scrollTop);
 
     return () => {
       headerTableBody.removeEventListener("mouseover", headerMouseFn);
@@ -119,10 +133,13 @@ const CustomTable = (props) => {
     };
   });
 
-  const columnsChild = columns.filter((item) => item.children);
+  // 头部行数
+  const rowSpan = useMemo(function () {
+    const columnsChild = columns.filter((item) => item.children);
+    return columnsChild.length > 0 ? 2 : 1;
+  }, [columns])
 
-  const rowSpan = columnsChild.length > 0 ? 2 : 1;
-
+  // 头部第二行Cell
   const HeaderCell = ({ childColumns }) => {
     return childColumns.map((item) => (
       <th key={item.dataIndex}>
@@ -160,16 +177,15 @@ const CustomTable = (props) => {
             />
             <div className={style.viewContent}>
               {dataList.map((item, index) => (
-                <div className={style.viewItem} style={{ position: 'absolute', top: 0, transform: `translate3d(0, ${transform + index * rowHeight}px, 0)` }}>
+                <div className={style.viewItem} style={{ position: 'absolute', top: 0, transform: `translate3d(0, ${item.transform}px, 0)` }}>
                   <TableCell data={item} columns={leftColumns} key={item.id} />
                 </div>
               ))}
             </div>
           </div>
-          <div className={style.tableFooter}>尾巴</div>
         </div>
       </div>
-      <div className={style.table}>
+      <div className={classnames(style.table, style.mainTable)}>
         <div className={style.tableHeader} ref={virtualHeader}>
           <table border="1">
             <tbody>
@@ -207,13 +223,12 @@ const CustomTable = (props) => {
           />
           <div className={style.viewContent}>
             {dataList.map((item, index) => (
-              <div className={style.viewItem} style={{ position: 'absolute', top: 0, transform: `translate3d(0, ${transform + index * rowHeight}px, 0)` }}>
+              <div className={style.viewItem} style={{ position: 'absolute', top: 0, transform: `translate3d(0, ${item.transform}px, 0)` }}>
                 <TableCell data={item} columns={columns} key={item.id} />
               </div>
             ))}
           </div>
         </div>
-        <div className={style.tableFooter}>尾巴</div>
       </div>
     </div>
   );
